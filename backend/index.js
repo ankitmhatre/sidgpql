a72010e777eec65727f91d48f976dab4a9ca70cf
Ankit <ankitmhatre.mhatre9@gmail.com>
2022-11-05 17:00:02 -0400

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const http = require("http");
+const app = express();
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+
+let apolloServer = null;
+async function startServer() {
+    apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+}
+startServer();
+const httpserver = http.createServer(app);
+
+app.get("/rest", function (req, res) {
+    res.json({ data: "api working" });
+});
+
+app.listen(4000, function () {
+    console.log(`server running on port 4000`);
+    console.log(`gql path is ${apolloServer.graphqlPath}`);
+});

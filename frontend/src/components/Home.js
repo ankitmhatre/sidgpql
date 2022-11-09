029f872aa2f457e427594555d86428fb79523a50
Ankit <ankitmhatre.mhatre9@gmail.com>
2022-11-09 12:48:06 -0500

    prevent scroll reset

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,43 +6,43 @@ import ImagePost from "./ImagePost";
 import { ScrollRestoration } from "react-router-dom";
 
 export default function Home() {
-
   const [pageNum, setPageNum] = useState(1);
   const [images, setImages] = useState([]);
   const [scrollPos, setScrollPos] = useState(0);
-  const { loading, error, data, refetch } = useQuery(queries.UNSPLASH, {    
+  const { loading, error, data, refetch } = useQuery(queries.UNSPLASH, {
     variables: { pageNum },
     fetchPolicy: "cache-and-network",
   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :</p>;
-
-
-
-  function loadMore(){
-    setPageNum(pageNum+1);
-setScrollPos(window.scrollY)
+  function loadMore() {
+    setPageNum(pageNum + 1);
+    setScrollPos(window.scrollY);
     refetch();
-   window.scrollTo(0, scrollPos)
+    
+  }
+  console.log(data)
+
+  if(data!==undefined && data !==null && data.unsplashImages!==undefined){
+    data.unsplashImages.map((k, v) => {
+             images.push(k);
+             })
+
+             window.scrollTo(0, scrollPos);
+  }
+
+  if (error) {
+    console.log(error)
+
   }
   return (
-  <>
-
- 
-      {
-        data.unsplashImages.map((k, v) => {
-        images.push(k)
+    <>
+      {images.map((k, v) => {
+        return <ImagePost k={k} />;
       })}
-
-
-      {images.map((k,v)=>{
-        return (
-         <ImagePost k={k} />
-        );
-      })}
-
-      <button  onClick={loadMore}>Load More</button>
-  </>
+{
+    loading ? (<span>Loading......</span>) : <button onClick={loadMore}>Load More</button>
+}
+      
+    </>
   );
 }

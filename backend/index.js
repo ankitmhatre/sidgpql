b78b5cfd79324f758df61a71e389ffb74639ec17
Ankit <ankitmhatre.mhatre9@gmail.com>
2022-11-08 21:01:02 -0500

    add unpslash

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,265 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
 const http = require("http");
 const app = express();
+var path = require("path");
+const fs = require("fs");
+var axios = require("axios");
+const redis = require("redis");
+const redisClient = redis.createClient();
+
+const cors = require("cors");
+app.use(cors());
+
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+redisClient.connect().then(() => {
+  console.log("Connected!");
+});
+
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello(name: String): String
+    unsplashImages(pageNum: Int): [ImagePost]
+    userPostedImages: [ImagePost]
+  }
+  type ImagePost {
+    id: ID!
+    url: String!
+    posterName: String!
+    description: String
+    userPosted: Boolean!
+    binned: Boolean!
+  }
+  type Mutation {
+    updateImage(
+      id: ID!
+      url: String
+      posterName: String
+      description: String
+      userPosted: Boolean
+      binned: Boolean
+    ): ImagePost
+
+    helloName(name: String!): String
+    uploadImage(
+      url: String!
+      description: String
+      posterName: String
+    ): ImagePost
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    hello: () => {
+      var a = "Hello";
+      return a;
+    },
+
+    userPostedImages: async () => {
+      let returnData = [];
+      const data = await redisClient.lRange("photos_list", 0, -1);
+      if (data.length != 0) {
+        for (let err of data) {
+          const jsonImageFromRedis = await redisClient.getAsync(err);
+          const recomposedImage = JSON.parse(jsonImageFromRedis);
+          returnData.push(recomposedImage);
+        }
+      }
+      return returnData;
+    },
+
+    async unsplashImages(_, args) {
+      let returnData = [];
+      let imageData = {};
+
+      try {
+        const { data } = await axios.get(
+          "https://api.unsplash.com/photos?page=" +
+            args.pageNum +
+            "&client_id=FUANkq2fJtG-qRGo9nfn0w1_dbXDtY7xhe4F_yR9Kq4"
+        );
+
+        let existBin;
+        for (let arr of data) {
+          imageData.id = arr.id;
+          imageData.url = arr.urls.thumb;
+          imageData.posterName = arr.user.name ? arr.user.name : "No Author";
+          imageData.description = arr.description
+            ? arr.description
+            : arr.alt_description;
+          imageData.userPosted = false;
+
+          console.log(arr.id)
+          existBin = await redisClient.get(arr.id);
+          if (existBin) imageData.binned = true;
+          else imageData.binned = false;
+       //   imageData.numBinned = arr.likes;
+          returnData.push(imageData);
+          imageData = {};
+        }
+      } catch (err) {
+      console.log(err)
+        return null;
+      }
+
+      return returnData;
+    },
+  },
+  Mutation: {
+    helloName: async (root, args, context) => {
+      const { name } = args;
+      return "Hello " + name;
+    },
+    async uploadImage(_, args) {
+      const redisId = uuid.v4();
+      const newImage = {
+        id: redisId,
+        url: args.url,
+        posterName: args.posterName,
+        description: args.description,
+        userPosted: true,
+        binned: false,
+      };
+
+      redisClient.rPush("photos_list", redisId, (err, _data) => {
+        if (err) {
+          return null;
+        }
+      });
+
+      const jsonBio = JSON.stringify(newImage);
+      await redisClient.setAsync(redisId, jsonBio);
+
+      const jsonImageFromRedis = await redisClient.getAsync(redisId);
+      const recomposedImage = JSON.parse(jsonImageFromRedis);
+
+      return recomposedImage;
+    },
+
+    async updateImage(_, args) {
+      let redisId = args.id;
+      if (args.binned) {
+        const newImage = {
+          id: redisId,
+          url: args.url,
+          posterName: args.posterName,
+          description: args.description,
+          userPosted: args.userPosted,
+          binned: args.binned,
+        
+        };
+
+        /* client.rpush('binnedImages', redisId, (err, _data) => {
+        if(err) {
+            return null;
+        }
+      })*/
+
+       
+
+        await redisClient.delAsync(redisId);
+        const jsonBio = JSON.stringify(newImage);
+        await redisClient.setAsync(redisId, jsonBio);
+        const jsonImageFromRedis = await redisClient.getAsync(redisId);
+        const recomposedImage = JSON.parse(jsonImageFromRedis);
+
+        return recomposedImage;
+      } else {
+        /* client.lrem('binnedImages', 0, redisId, function(err, data){
+     if(err) {
+       return null;
+    }*/
+
+        //redisClient.zrem("binnedImages", redisId);
+
+        const jsonImageFromRedis = await redisClient.getAsync(redisId);
+        const recomposedImage = JSON.parse(jsonImageFromRedis);
+
+        await redisClient.delAsync(redisId);
+        return recomposedImage;
+      }
+    },
   },
 };
 
-
 let apolloServer = null;
 async function startServer() {
-    apolloServer = new ApolloServer({
-        typeDefs,
-        resolvers,
-    });
-    await apolloServer.start();
-    apolloServer.applyMiddleware({ app });
+  apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 }
 startServer();
 const httpserver = http.createServer(app);
 
 app.get("/rest", function (req, res) {
-    res.json({ data: "api working" });
+  res.json({ data: "api working" });
+});
+
+app.get("/j", function (req, res) {
+  fs.readFile(
+    path.join(__dirname, "./assets", "placeholder.json"),
+    "utf8",
+    (err, jsonString) => {
+      if (err) {
+        console.log("File read failed:", err);
+        return;
+      }
+      //   console.log('File data:', jsonString)
+
+      var l = JSON.parse(jsonString);
+
+      res.json(l);
+    }
+  );
+});
+
+app.get("/photos", async (req, res) => {
+  let cachedData = await redisClient.exists("photos_list");
+
+  if (cachedData) {
+    console.log("Data from Cache");
+    let showDetailPage = await redisClient.get("photos_list");
+    res.send(JSON.parse(showDetailPage));
+
+    return;
+  } else {
+    console.log("Data from Api");
+    //const unsplashApiResponse = [{id : 1}]
+    const unsplashApiResponse = await axios.get(
+      "https://api.unsplash.com/photos?client_id=FUANkq2fJtG-qRGo9nfn0w1_dbXDtY7xhe4F_yR9Kq4"
+    );
+    var r = unsplashApiResponse.data;
+
+    try {
+      await redisClient.set("photos_list", JSON.stringify(r));
+
+      res.json(r);
+    } catch (err) {
+      console.log(err);
+
+      res.status(404).json({ err });
+    }
+  }
+  return;
 });
 
 app.listen(4000, function () {
-    console.log(`server running on port 4000`);
-    console.log(`gql path is ${apolloServer.graphqlPath}`);
+  console.log(`server running on port 4000`);
+  console.log(`gql path is ${apolloServer.graphqlPath}`);
 });
+
+//await
+// redisClient.sadd(
+//   ["photos_list", "ReactJS", "Angular", "Svelte", "VueJS", "VueJS"],
+//   function (err, reply) {
+//     console.log(reply); // 4
+//   }
+// );
